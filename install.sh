ac81528ff9c90808c3a9ddb10dcef38e14b4a8e4
Deepak Kumar <dee.kumar@gmail.com>
2026-03-10 16:59:49 -0700

    Added git config

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 echo $SCRIPT_DIR
 
-for file in aliases bash_profile bashrc exports path Brewfile gemrc; do
+for file in aliases bash_profile bashrc exports path Brewfile gemrc gitconfig gitignore; do
   if ln -s $SCRIPT_DIR/$file ~/.$file; then
     echo "Linked $SCRIPT_DIR/$file to ~/.$file"
   else
