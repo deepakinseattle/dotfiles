05b0b27f9f844dfa0959072bc4ddb445da139bb0
Deepak Kumar <dee.kumar@gmail.com>
2026-03-10 16:31:45 -0700

    Initial rewrite

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+echo $SCRIPT_DIR
+
+for file in aliases bash_profile bashrc exports path; do
+  if ln -s $SCRIPT_DIR/$file ~/.$file; then
+    echo "Linked $SCRIPT_DIR/$file to ~/.$file"
+  else
+    echo "Unable to link $SCRIPT_DIR/$file to ~/.$file"
+  fi
+done;
+unset $file;
