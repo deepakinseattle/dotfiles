a094414fa00e5c5ff77d5ab545ee92ccd427f510
Deepak Kumar <dee.kumar@gmail.com>
2026-03-12 10:06:41 -0700

    Added telescope

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -17,5 +17,16 @@ vim.opt.expandtab = true
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
+vim.opt.clipboard = unnamedplus
 
 vim.keymap.set('n', '<leader>e', '<cmd>NvimTreeToggle<CR>', { desc = "Toggle file explorer" })
+
+local builtin = require('telescope.builtin')
+vim.keymap.set("n", "<leader>ff", function()
+    builtin.find_files({ hidden = true, follow = true })
+end, { desc = "Telescope [f]ind [f]iles" })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+
